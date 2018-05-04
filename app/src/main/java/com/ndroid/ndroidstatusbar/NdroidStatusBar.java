91ed348e7e95dfecb2be7a919b0fd76dda73ceab
Octavian Bostan <octavian.bostan@windriver.com>
2018-05-04 10:16:38 +0300

    Transparent background.

--- app/src/main/java/com/ndroid/ndroidstatusbar/NdroidStatusBar.java
+++ app/src/main/java/com/ndroid/ndroidstatusbar/NdroidStatusBar.java
@@ -19,10 +19,13 @@ public class NdroidStatusBar extends RelativeLayout {
     private Context mContext;
 
     // Dimensions
-    private int BAR_HEIGHT = 100;
+    private int BAR_HEIGHT = 90;
     private int ICON_SIZE = 60;
     private int ICON_MARGIN  = 25;
     private int ICON_MARGIN_END = 50;
+
+    private RelativeLayout mSettingsLayout;
+    private RelativeLayout mIconLayout;
 
     // Carrier
     private TextView mCarrier;
@@ -56,15 +59,33 @@ public class NdroidStatusBar extends RelativeLayout {
         Log.d(TAG, "NdroidStatusBar()");
 
         mContext = context;
-        setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, BAR_HEIGHT));
-        setBackground(ContextCompat.getDrawable(context, R.drawable.status_bar_shape));
+
+        initSettingsLayout();
+        initIconLayout();
+
+        setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        addView(mSettingsLayout);
+        addView(mIconLayout);
+    }
+
+    private void initSettingsLayout() {
+        mSettingsLayout = new RelativeLayout(mContext);
+    }
+
+
+    private void initIconLayout() {
+        RelativeLayout.LayoutParams iparam= new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, BAR_HEIGHT);
+        mIconLayout = new RelativeLayout(mContext);
+        mIconLayout.setLayoutParams(iparam);
+        setBackground(ContextCompat.getDrawable(mContext, R.drawable.status_bar_shape));
+
 
         // Battery Level
         mBatteryView  = new View(mContext);
         mBatteryView.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT,
                 mBatteryLevel));
-        mBatteryView.setBackground(ContextCompat.getDrawable(context, R.drawable.battery_view_green));
-        addView(mBatteryView);
+        mBatteryView.setBackground(ContextCompat.getDrawable(mContext, R.drawable.battery_view_green));
+        mIconLayout.addView(mBatteryView);
 
         // Carrier
         mCarrier = new TextView(mContext);
@@ -76,7 +97,8 @@ public class NdroidStatusBar extends RelativeLayout {
         mCarrier.setLayoutParams(cParams);
         cParams.setMarginStart(ICON_MARGIN_END);
         mCarrier.setTextColor(Color.BLACK);
-        addView(mCarrier);
+        mCarrier.setBackgroundColor(Color.TRANSPARENT);
+        mIconLayout.addView(mCarrier);
 
         // Clock
         mClockText = new TextView(mContext);
@@ -85,10 +107,11 @@ public class NdroidStatusBar extends RelativeLayout {
                 LayoutParams.WRAP_CONTENT);
         params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
         mClockText.setLayoutParams(params);
+        mClockText.setBackgroundColor(Color.TRANSPARENT);
         mClockText.setTextColor(Color.BLACK);
         mClockText.setTypeface(mClockText.getTypeface(), Typeface.BOLD);
         mClockText.setId(mClockId);
-        addView(mClockText);
+        mIconLayout.addView(mClockText);
 
 
         // Ringtone
@@ -98,9 +121,9 @@ public class NdroidStatusBar extends RelativeLayout {
         rParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
         rParams.setMarginEnd(ICON_MARGIN_END);
         mRingtone.setLayoutParams(rParams);
-        mRingtone.setBackground(ContextCompat.getDrawable(context, R.drawable.ic_volume_up));
+        mRingtone.setBackground(ContextCompat.getDrawable(mContext, R.drawable.ic_volume_up));
         mRingtone.setId(mRingtoneId);
-        addView(mRingtone);
+        mIconLayout.addView(mRingtone);
 
         // Wifi
         mWifi = new View(mContext);
@@ -109,9 +132,9 @@ public class NdroidStatusBar extends RelativeLayout {
         wParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
         wParams.setMarginEnd(ICON_MARGIN);
         mWifi.setLayoutParams(wParams);
-        mWifi.setBackground(ContextCompat.getDrawable(context, R.drawable.ic_wifi));
+        mWifi.setBackground(ContextCompat.getDrawable(mContext, R.drawable.ic_wifi));
         mWifi.setId(mWifiId);
-        addView(mWifi);
+        mIconLayout.addView(mWifi);
 
         // Bluetooth
         mBluetooth = new View(mContext);
@@ -120,16 +143,16 @@ public class NdroidStatusBar extends RelativeLayout {
         bParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
         bParams.setMarginEnd(ICON_MARGIN);
         mBluetooth.setLayoutParams(bParams);
-        mBluetooth.setBackground(ContextCompat.getDrawable(context, R.drawable.ic_bluetooth_yes));
+        mBluetooth.setBackground(ContextCompat.getDrawable(mContext, R.drawable.ic_bluetooth_yes));
         mBluetooth.setId(mBluetoothId);
-        addView(mBluetooth);
+        mIconLayout.addView(mBluetooth);
     }
 
     public void setBatteryLevel(int level) {
         Log.d(TAG, "setBatteryLevel() " + level);
 
         mBatteryLevel = level;
-        removeView(mBatteryView);
+        mIconLayout.removeView(mBatteryView);
         mBatteryView  = new View(mContext);
         mBatteryView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
                 mBatteryLevel));
@@ -141,7 +164,7 @@ public class NdroidStatusBar extends RelativeLayout {
             mBatteryView.setBackground(ContextCompat.getDrawable(mContext, R.drawable.battery_view_red));
         }
 
-        addView(mBatteryView);
+        mIconLayout.addView(mBatteryView);
 
         // Brint text and icons on top of battery level
         bringViewsToFront();
@@ -149,18 +172,16 @@ public class NdroidStatusBar extends RelativeLayout {
 
     public void setWifi(boolean status){
         Log.d(TAG, "setWifi() " + status);
-        removeView(mWifi);
         if (status) {
             mWifi.setVisibility(VISIBLE);
         } else {
             mWifi.setVisibility(GONE);
         }
-        addView(mWifi);
     }
 
     public void setRingtone(int ringtone) {
         Log.d(TAG, "setRingtone() " + ringtone);
-        removeView(mRingtone);
+        mIconLayout.removeView(mRingtone);
         switch (ringtone) {
             case ON:
                 mRingtone.setBackground(ContextCompat.getDrawable(mContext, R.drawable.ic_volume_up));
@@ -174,18 +195,16 @@ public class NdroidStatusBar extends RelativeLayout {
             default:
                 break;
         }
-        addView(mRingtone);
+        mIconLayout.addView(mRingtone);
     }
 
     public void setBluetooth(boolean status) {
         Log.d(TAG, "setBluetooth() " + status);
-        removeView(mBluetooth);
         if (status) {
-            mBluetooth.setBackground(ContextCompat.getDrawable(mContext, R.drawable.ic_bluetooth_yes));
+            mBluetooth.setVisibility(View.VISIBLE);
         } else {
-            mBluetooth.setBackground(ContextCompat.getDrawable(mContext, R.drawable.ic_bluetooth_no));
+            mBluetooth.setVisibility(View.GONE);
         }
-        addView(mBluetooth);
     }
 
     private void bringViewsToFront() {
